9dca11533a78753659f7f598e9ac9695097fa484
Frans de Boer <frans-de-boer@hotmail.com>
2021-11-28 18:58:59 +0100

    finished util, removed old keyboard layout

--- keymap.c
+++ keymap.c
@@ -9,7 +9,7 @@
  * .--------------------------------------.  .--------------------------------------.
  * |   Q   |   W   |   E   |   R   |   T  |  |   Y   |   U   |   I   |   O   |   P  |
  * |-------+-------+-------+-------+------|  |-------+-------+-------+-------+------|
- * |   A   |   S   |   D   |   F   |   G  |  |   H   |   J   |   K   |   L   |   '  |
+ * |   A   |   S   |   D   |   F   |   G  |  |   H   |   J   |   K   |   L   | ""/' |
  * |-------+-------+-------+-------+------|  |-------+-------+-------+-------+------|
  * |   Z   |   X   |   C   |   V   |   B  |  |   N   |   M   |   ,   |   .   |   /  |
  * '---------------+-------+-------+------|  |-------+-------+-------+-------+------'
@@ -19,7 +19,7 @@
  * Alpha (Colemak DH)
  * Alpha keys plus some writing special characters (,./). The semicolon (;/:) is replaced with apostraphes ('/")
  * .--------------------------------------.  .--------------------------------------.
- * |   Q   |   W   |   F   |   P   |   B  |  |   J   |   L   |   U   |   Y   |   '  |
+ * |   Q   |   W   |   F   |   P   |   B  |  |   J   |   L   |   U   |   Y   | ""/' |
  * |-------+-------+-------+-------+------|  |-------+-------+-------+-------+------|
  * |   A   |   R   |   S   |   T   |   G  |  |   M   |   N   |   E   |   I   |   O  |
  * |-------+-------+-------+-------+------|  |-------+-------+-------+-------+------|
@@ -30,7 +30,7 @@
  *
  * Utils
  * .---------------------------------------.  .---------------------------------------.
- * |       |  7/&  |  8/*  |   9   |       |  |       |  {}   |  -/_  | ""/'' |  '    |
+ * |       |  7/&  |  8/*  |   9   |       |  |       |  {}   |  -/_  |   `   | ""/'  |
  * |-------+-------+-------+-------+-------|  |-------+-------+-------+-------+-------|
  * |       |  4/$  |  5/%  |  6/^  |       |  |  ;/:  |  ()   |  =/+  |  <>   |   \   |
  * |-------+-------+-------+-------+-------|  |-------+-------+-------+-------+-------|
@@ -76,16 +76,23 @@ Format (with homerow mods):
 ),
 */
 
-// Layers
-#define QWERTY 0
-#define COLEMAK 1
-#define UTIL 2
-#define NAV 3
+enum layers {
+    QWERTY,
+    COLEMAK,
+    UTIL,
+    NAV,
+}
+enum custom_keycodes {
+    PRN = SAFE_RANGE
+    BRC,
+    AGL_BRC,
+    SQL_BRC,
+}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [QWERTY] = LAYOUT_ergodox(
         // left hand
-        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+        DF(COLEMAK),    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
         KC_NO,          KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_NO,
         KC_NO,          LALT_T(KC_A),   LCTL_T(KC_S),   LSFT_T(KC_D),   LGUI_T(KC_F),   KC_G,
         KC_NO,          KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,          KC_NO,
@@ -99,14 +106,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_NO,
                         KC_H,           LGUI_T(KC_J),   LSFT_T(KC_K),   LCTL_T(KC_L),   LALT_T(KC_QUOT),KC_NO,
         KC_NO,          KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,        KC_NO,
-                                        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+                                        TG(UTIL),       KC_NO,          KC_NO,          KC_NO,          KC_NO,
         KC_NO,          KC_NO,
         KC_NO,
         KC_NO,          KC_TAB,         KC_ENT
     ),
     [COLEMAK] = LAYOUT_ergodox(
         // left hand
-        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+        DF(QWERTY),     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
         KC_NO,          KC_Q,           KC_W,           KC_F,           KC_P,           KC_B,           KC_NO,
         KC_NO,          LALT_T(KC_A),   LCTL_T(KC_R),   LSFT_T(KC_S),   LGUI_T(KC_T),   KC_G,
         KC_NO,          KC_Z,           KC_X,           KC_C,           KC_D,           KC_V,          KC_NO,
@@ -138,12 +145,69 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                         KC_SPC,         KC_BSPC,        KC_NO,
         // right hand
         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-                        KC_NO,          LGUI_T(KC_NO),  LSFT_T(KC_NO),  LCTL_T(KC_NO),  LALT_T(KC_NO),  KC_NO,
-        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+        KC_NO,          KC_NO,          SQL_BRC,        KC_MINS,        KC_GRAVE,       KC_QUOT,        KC_NO,
+                        KC_SCLN,        LGUI_T(PRN),    LSFT_T(KC_EQL), LCTL_T(AGL_BRC),LALT_T(KC_BSLS),KC_NO,
+        KC_NO,          KC_NO,          BRC,            KC_COMM,        KC_DOT,         KC_SLSH,        KC_NO,
                                         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
         KC_NO,          KC_NO,
         KC_NO,
         KC_NO,          KC_TAB,         KC_ENT
     ),
- }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    static uint16_t code_timer;
+    switch (keycode) {
+        case PRN:
+            if (record->event.pressed) {
+                SEND_STRING("()"SS_TAP(X_LEFT));
+            }
+            return false;
+            break;
+
+        case BRC:
+            if (record->event.pressed) {
+                SEND_STRING("[]"SS_TAP(X_LEFT));
+            }
+            return false;
+            break;
+
+        case SQL_BRC:
+            if (record->event.pressed) {
+                SEND_STRING("{}"SS_TAP(X_LEFT));
+            }
+            return false;
+            break;
+
+        case AGL_BRC:
+            if (record->event.pressed) {
+                SEND_STRING("<>"SS_TAP(X_LEFT));
+            }
+            return false;
+            break;
+    }
+}
+
+// Runs constantly in the background, in a loop.
+void matrix_scan_user(void) {
+
+    // uint8_t layer = biton32(layer_state);
+
+    ergodox_board_led_off();
+    ergodox_right_led_1_off();
+    ergodox_right_led_2_off();
+    ergodox_right_led_3_off();
+    // switch (layer) {
+    //   // TODO: Make this relevant to the ErgoDox EZ.
+    //     case 1:
+    //         ergodox_right_led_1_on();
+    //         break;
+    //     case 2:
+    //         ergodox_right_led_2_on();
+    //         break;
+    //     default:
+    //         // none
+    //         break;
+    // }
+
+};
