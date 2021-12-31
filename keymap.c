7bfd181f8187b3d0bccef99aa18ec20a79fb0e19
Frans de Boer <frans-de-boer@hotmail.com>
2021-12-31 17:09:01 +0100

    started work on new layout

--- keymap.c
+++ keymap.c
@@ -16,21 +16,9 @@
  *                 |  Nav  | Space |Bspace|  |  Tab  | Enter | Utils |
  *                 '----------------------'  '-----------------------'
  *
- * Alpha (Colemak DH)
- * Alpha keys plus some writing special characters (,./). The semicolon (;/:) is replaced with apostraphes ('/")
- * .--------------------------------------.  .--------------------------------------.
- * |   Q   |   W   |   F   |   P   |   B  |  |   J   |   L   |   U   |   Y   | ""/' |
- * |-------+-------+-------+-------+------|  |-------+-------+-------+-------+------|
- * |   A   |   R   |   S   |   T   |   G  |  |   M   |   N   |   E   |   I   |   O  |
- * |-------+-------+-------+-------+------|  |-------+-------+-------+-------+------|
- * |   Z   |   X   |   C   |   D   |   V  |  |   K   |   H   |   ,   |   .   |   /  |
- * '---------------+-------+-------+------|  |-------+-------+-------+-------+------'
- *                 |  Nav  | Space |Bspace|  |  Tab  | Enter | Utils |     
- *                 '----------------------'  '-----------------------'
- *
  * Utils
  * .---------------------------------------.  .---------------------------------------.
- * |       |  7/&  |  8/*  |   9   |       |  |       |  {}   |  -/_  |   `   | ""/'  |
+ * |       |  7/&  |  8/ * |   9   |       |  |       |  {}   |  -/_  |   `   | ""/'  |
  * |-------+-------+-------+-------+-------|  |-------+-------+-------+-------+-------|
  * |       |  4/$  |  5/%  |  6/^  |       |  |  ;/:  |  ()   |  =/+  |  <>   |   \   |
  * |-------+-------+-------+-------+-------|  |-------+-------+-------+-------+-------|
@@ -41,7 +29,7 @@
  *
  * Navigation
  * .---------------------------------------.  .---------------------------------------.
- * |       |       |       |       |       |  |       |  Cut  | Copy  | Paste |       |
+ * |       |       |       |       |       |  |       |  Copy | Paste |  Cut  |       |
  * |-------+-------+-------+-------+-------|  |-------+-------+-------+-------+-------|
  * |       |       |       |       |       |  |       |  <-   |   v   |   ^   |  ->   |
  * |-------+-------+-------+-------+-------|  |-------+-------+-------+-------+-------|
@@ -76,27 +64,42 @@ Format (with homerow mods):
 ),
 */
 
+#include QMK_KEYBOARD_H
+#include "version.h"
+
 enum layers {
     QWERTY,
-    COLEMAK,
     UTIL,
     NAV,
-}
+};
 enum custom_keycodes {
-    PRN = SAFE_RANGE
-    BRC,
-    AGL_BRC,
-    SQL_BRC,
-}
+    FULL_SCRN = SAFE_RANGE,
+    // LEFT_SCRN,
+    // RIGHT_SCRN,
+    // TOP_SCRN,
+    // BTM_SCRN,
+    // TL_SCRN,
+    // TR_SCRN,
+    // BL_SCRN,
+    // BR_SCRN,
+    // PRN = SAFE_RANGE,
+    // BRC,
+    // AGL_BRC,
+    // SQL_BRC,
+
+    // CUT,
+    // COPY,
+    // PASTE
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [QWERTY] = LAYOUT_ergodox(
         // left hand
-        DF(COLEMAK),    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
         KC_NO,          KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_NO,
         KC_NO,          LALT_T(KC_A),   LCTL_T(KC_S),   LSFT_T(KC_D),   LGUI_T(KC_F),   KC_G,
         KC_NO,          KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,          KC_NO,
-        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+        KC_NO,          KC_NO,          KC_NO,          KC_NO,          TG(NAV),
 
                                                                                         KC_NO,          KC_NO,
                                                                                                         KC_NO,
@@ -111,27 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,
         KC_NO,          KC_TAB,         KC_ENT
     ),
-    [COLEMAK] = LAYOUT_ergodox(
-        // left hand
-        DF(QWERTY),     KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-        KC_NO,          KC_Q,           KC_W,           KC_F,           KC_P,           KC_B,           KC_NO,
-        KC_NO,          LALT_T(KC_A),   LCTL_T(KC_R),   LSFT_T(KC_S),   LGUI_T(KC_T),   KC_G,
-        KC_NO,          KC_Z,           KC_X,           KC_C,           KC_D,           KC_V,          KC_NO,
-        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
 
-                                                                                        KC_NO,          KC_NO,
-                                                                                                        KC_NO,
-                                                                        KC_SPC,         KC_BSPC,        KC_NO,
-        // right hand
-        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-        KC_NO,          KC_J,           KC_L,           KC_U,           KC_Y,           KC_QUOT,        KC_NO,
-                        KC_M,           LGUI_T(KC_N),   LSFT_T(KC_E),   LCTL_T(KC_I),   LALT_T(KC_O),   KC_NO,
-        KC_NO,          KC_K,           KC_H,           KC_COMM,        KC_DOT,         KC_SLSH,        KC_NO,
-                                        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-        KC_NO,          KC_NO,
-        KC_NO,
-        KC_NO,          KC_TAB,         KC_ENT
-    ),
     [UTIL] = LAYOUT_ergodox(
         // left hand
         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
@@ -145,69 +128,98 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                         KC_SPC,         KC_BSPC,        KC_NO,
         // right hand
         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
-        KC_NO,          KC_NO,          SQL_BRC,        KC_MINS,        KC_GRAVE,       KC_QUOT,        KC_NO,
-                        KC_SCLN,        LGUI_T(PRN),    LSFT_T(KC_EQL), LCTL_T(AGL_BRC),LALT_T(KC_BSLS),KC_NO,
-        KC_NO,          KC_NO,          BRC,            KC_COMM,        KC_DOT,         KC_SLSH,        KC_NO,
+        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+                        KC_NO,          LGUI_T(KC_NO),  LSFT_T(KC_NO),  LCTL_T(KC_NO),  LALT_T(KC_NO),  KC_NO,
+        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+                                        TG(UTIL),       KC_NO,          KC_NO,          KC_NO,          KC_NO,
+        KC_NO,          KC_NO,
+        KC_NO,
+        KC_NO,          KC_TAB,         KC_ENT
+    ),
+
+    [NAV] = LAYOUT_ergodox(
+        // left hand
+        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+        KC_NO,          LALT_T(KC_NO),  LCTL_T(KC_NO),   LSFT_T(FULL_SCRN),  LGUI_T(KC_NO),  KC_NO,
+        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+        KC_NO,          KC_NO,          KC_NO,          KC_NO,          TG(NAV),
+
+                                                                                        KC_NO,          KC_NO,
+                                                                                                        KC_NO,
+                                                                        KC_SPC,         KC_BSPC,        KC_NO,
+        // right hand
+        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
+                        KC_NO,          KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_NO,
+        KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
                                         KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
         KC_NO,          KC_NO,
         KC_NO,
         KC_NO,          KC_TAB,         KC_ENT
     ),
-}
+};
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    static uint16_t code_timer;
     switch (keycode) {
-        case PRN:
+        case FULL_SCRN:
             if (record->event.pressed) {
-                SEND_STRING("()"SS_TAP(X_LEFT));
+                register_code(KC_LGUI);
+                register_code(KC_LALT);
+                SEND_STRING("f");
+                clear_mods();
             }
-            return false;
-            break;
-
-        case BRC:
-            if (record->event.pressed) {
-                SEND_STRING("[]"SS_TAP(X_LEFT));
-            }
-            return false;
-            break;
-
-        case SQL_BRC:
-            if (record->event.pressed) {
-                SEND_STRING("{}"SS_TAP(X_LEFT));
-            }
-            return false;
-            break;
-
-        case AGL_BRC:
-            if (record->event.pressed) {
-                SEND_STRING("<>"SS_TAP(X_LEFT));
-            }
-            return false;
-            break;
+            return true;
+        // case LEFT_SCRN:
+        //     if (record->event.pressed) {
+        //         register_code(KC_LGUI);
+        //         register_code(KC_LALT);
+        //         tap_code(KC_LEFT);
+        //         clear_mods();
+        //     }
+        //     return false;
+        // case RIGHT_SCRN:
+        //     if (record->event.pressed) {
+        //         register_code(KC_LGUI);
+        //         register_code(KC_LALT);
+        //         tap_code(KC_RIGHT);
+        //         clear_mods();
+        //     }
+        //     return false;
+        // case TOP_SCRN:
+        //     if (record->event.pressed) {
+        //         register_code(KC_LGUI);
+        //         register_code(KC_LALT);
+        //         tap_code(KC_UP);
+        //         clear_mods();
+        //     }
+        //     return false;
+        // case BTM_SCRN:
+        //     if (record->event.pressed) {
+        //         register_code(KC_LGUI);
+        //         register_code(KC_LALT);
+        //         tap_code(KC_DOWN);
+        //         clear_mods();
+        //     }
+        //     return false;
+        default:
+            return true;
     }
 }
 
-// Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {
-
-    // uint8_t layer = biton32(layer_state);
+    uint8_t layer = biton32(layer_state);
 
     ergodox_board_led_off();
     ergodox_right_led_1_off();
     ergodox_right_led_2_off();
     ergodox_right_led_3_off();
-    // switch (layer) {
-    //   // TODO: Make this relevant to the ErgoDox EZ.
-    //     case 1:
-    //         ergodox_right_led_1_on();
-    //         break;
-    //     case 2:
-    //         ergodox_right_led_2_on();
-    //         break;
-    //     default:
-    //         // none
-    //         break;
-    // }
-
+    switch (layer) {
+        case UTIL:
+            ergodox_right_led_1_on();
+            break;
+        case NAV:
+            ergodox_right_led_2_on();
+            break;
+    }
 };
